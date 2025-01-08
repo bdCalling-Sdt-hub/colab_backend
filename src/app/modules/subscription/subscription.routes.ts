3fcb7916c1b0272221a83fedcaf15dd5e8eb435a
manik <maniksarker652@gmail.com>
2025-01-08 17:39:50 +0600

    route setup for renew subscription

--- src/app/modules/subscription/subscription.routes.ts
+++ src/app/modules/subscription/subscription.routes.ts
@@ -1,9 +1,17 @@
-import express from "express";
-import auth from "../../middlewares/auth";
-import { USER_ROLE } from "../user/user.constant";
-import SubscriptionController from "./subscription.controller";
-
+import express from 'express';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
+import SubscriptionController from './subscription.controller';
 
 const router = express.Router();
-router.post("/purchase-subscription",auth(USER_ROLE.user),SubscriptionController.purchaseSubscription);
+router.post(
+  '/purchase-subscription',
+  auth(USER_ROLE.user),
+  SubscriptionController.purchaseSubscription,
+);
+router.post(
+  '/renew-subscription',
+  auth(USER_ROLE.user),
+  SubscriptionController.renewSubscription,
+);
 export const subscriptionRoutes = router;
