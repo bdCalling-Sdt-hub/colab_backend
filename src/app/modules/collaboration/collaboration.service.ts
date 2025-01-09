adc6c199405cba9152e3d5b88f3b7b7c049833ae
manik <maniksarker652@gmail.com>
2025-01-09 16:59:37 +0600

    create service for get my collaboratin

--- src/app/modules/collaboration/collaboration.service.ts
+++ src/app/modules/collaboration/collaboration.service.ts
@@ -10,8 +10,17 @@ const sendCollaborationRequest = async (
   return result;
 };
 
+// get my collaboration
+const getMyCollaborations = async (profileId: string) => {
+  const result = await Collaboration.find({
+    $or: [{ sender: profileId }, { receiver: profileId }],
+  });
+  return result;
+};
+
 const CollaborationService = {
   sendCollaborationRequest,
+  getMyCollaborations,
 };
 
 export default CollaborationService;
