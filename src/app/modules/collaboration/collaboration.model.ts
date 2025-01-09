a156579d5994d95554c574f82f595eb5524ea4f1
manik <maniksarker652@gmail.com>
2025-01-09 14:59:15 +0600

    create model for collaboration

---
+++ src/app/modules/collaboration/collaboration.model.ts
@@ -0,0 +1,60 @@
+import mongoose, { Schema, Document } from 'mongoose';
+import { ICollaboration } from './collaboration.interface';
+
+// Define the Mongoose schema
+const CollaborationSchema = new Schema<ICollaboration>(
+  {
+    sender: {
+      type: Schema.Types.ObjectId,
+      ref: 'NormalUser',
+      required: true,
+    },
+    receiver: {
+      type: Schema.Types.ObjectId,
+      ref: 'User', // Reference the User model or adjust as needed
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    startTime: {
+      type: String,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
+    endTime: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    contactNumber: {
+      type: String,
+      required: true,
+    },
+    additionalNote: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Collaboration = mongoose.model<ICollaboration & Document>(
+  'Collaboration',
+  CollaborationSchema,
+);
+
+export default Collaboration;
