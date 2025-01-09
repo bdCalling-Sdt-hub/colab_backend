a000b7f2f47abc90ba6bdc97dd0aa2b3dae2ed1f
manik <maniksarker652@gmail.com>
2025-01-09 15:16:19 +0600

    route setup for collaboration

---
+++ src/app/modules/collaboration/collaboration.routes.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
+import CollaborationValidations from './collaboration.validation';
+import validateRequest from '../../middlewares/validateRequest';
+import CollaborationController from './collaboration.controller';
+
+const router = express.Router();
+
+router.post(
+  '/send-request',
+  auth(USER_ROLE.user),
+  validateRequest(CollaborationValidations.collaborationSchema),
+  CollaborationController.sendCollaborationRequest,
+);
+
+export const collaborationRoutes = router;
