cabf8dfa74a59f9b342c995436d58ffd80f07c89
manik <maniksarker652@gmail.com>
2025-01-09 17:09:06 +0600

    create service function for get all collaboraton

--- src/app/modules/collaboration/collaboration.service.ts
+++ src/app/modules/collaboration/collaboration.service.ts
@@ -1,3 +1,4 @@
+import QueryBuilder from '../../builder/QueryBuilder';
 import { ICollaboration } from './collaboration.interface';
 import Collaboration from './collaboration.model';
 
@@ -20,9 +21,33 @@ const getMyCollaborations = async (profileId: string) => {
   return result;
 };
 
+// get all collaboration
+const getAllCollaborations = async (query: Record<string, unknown>) => {
+  const collaborationQuery = new QueryBuilder(
+    Collaboration.find()
+      .populate({ path: 'sender' })
+      .populate({ path: 'receiver' }),
+    query,
+  )
+    .search(['title'])
+    .fields()
+    .filter()
+    .paginate()
+    .sort();
+
+  const result = collaborationQuery.modelQuery;
+  const meta = collaborationQuery.countTotal();
+
+  return {
+    result,
+    meta,
+  };
+};
+
 const CollaborationService = {
   sendCollaborationRequest,
   getMyCollaborations,
+  getAllCollaborations,
 };
 
 export default CollaborationService;
