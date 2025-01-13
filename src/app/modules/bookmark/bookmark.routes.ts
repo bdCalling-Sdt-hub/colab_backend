8bf6a69cdd12e942526ee730ea3ef121acab543a
manik <maniksarker652@gmail.com>
2025-01-13 17:31:21 +0600

    route setup for bookmark

--- src/app/modules/bookmark/bookmark.routes.ts
+++ src/app/modules/bookmark/bookmark.routes.ts
@@ -4,26 +4,15 @@ import auth from '../../middlewares/auth';
 import productBookmarkController from './product.bookmark.controller';
 
 const router = express.Router();
-
-// router.post(
-//   '/create-product-bookmark',
-//   auth(USER_ROLE.customer),
-//   productBookmarkController.createBookmark,
-// );
 router.post(
   '/add-delete-product-bookmark/:id',
-  auth(USER_ROLE.customer),
+  auth(USER_ROLE.user),
   productBookmarkController.productBookmarkAddDelete,
 );
 router.get(
   '/my-bookmark-products',
-  auth(USER_ROLE.customer),
+  auth(USER_ROLE.user),
   productBookmarkController.getMyBookmark,
-);
-router.delete(
-  '/delete-bookmark-product/:id',
-  auth(USER_ROLE.customer),
-  productBookmarkController.deleteBookmark,
 );
 
 export const productBookmarkRoutes = router;
