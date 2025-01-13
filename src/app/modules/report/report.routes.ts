30a52c8005d2b53229bb753918aed117fc31d8fe
manik <maniksarker652@gmail.com>
2025-01-13 09:53:57 +0600

    route setup for crate report

--- src/app/modules/report/report.routes.ts
+++ src/app/modules/report/report.routes.ts
@@ -1,3 +1,17 @@
 import express from 'express';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
+import validateRequest from '../../middlewares/validateRequest';
+import ReportValidations from './report.validation';
+import ReportController from './report.controller';
 
 const router = express.Router();
+
+router.post(
+  '/cretae-report',
+  auth(USER_ROLE.user),
+  validateRequest(ReportValidations.reportValidationSchema),
+  ReportController.createReport,
+);
+
+export const reportRoutes = router;
